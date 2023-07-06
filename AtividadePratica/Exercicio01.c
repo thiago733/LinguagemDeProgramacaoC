ad753de2c88786786445698e96267ac86d3ad176
Thiago Fortes <thiago733@gmail.com>
2023-07-06 09:43:31 -0400

    Finalizando o exercicio

--- AtividadePratica/Exercicio01.c
+++ AtividadePratica/Exercicio01.c
@@ -2,12 +2,18 @@
 int main () {
     char nome[50];
     int registroUnico;
+    int anoAtual = 2023;
+    int ultimoDigitoDoAnoAtual = anoAtual % 10;
+
     printf ("Insira seu nome: ");
     fgets (nome, 50, stdin);
     printf ("Insira seu RU: ");
-    scanf ("%i", &registroUnico);
-    printf ("Seu RU é: %i", registroUnico);
-    
-    return 0;
+    scanf ("%d", &registroUnico);
+    int ultimoDigitoDoRegistroUnico = registroUnico % 10;
+    if (ultimoDigitoDoRegistroUnico == ultimoDigitoDoAnoAtual)
+        printf ("Aluno %s, seu RU está autenticado.\n", nome);
+    else
+        printf ("Aluno %s, seu RU não está autenticado.\n", nome);
 
+    return 0;
 }
